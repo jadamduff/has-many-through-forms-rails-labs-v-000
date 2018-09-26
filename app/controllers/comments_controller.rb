e986a599e5a0002c7cccf926d4f8a03cdb12b02d
Adam Duff <j.adam.duff@gmail.com>
2018-09-26 17:18:56 +0000

    automatically backed up by learn

--- app/controllers/comments_controller.rb
+++ app/controllers/comments_controller.rb
@@ -1,13 +1,21 @@
 class CommentsController < ApplicationController
 
   def create
-    comment = Comment.create(comment_params)
-    redirect_to comment.post
+    @comment = Comment.new
+    @comment.content = params[:comment][:content]
+    @comment.post_id = params[:comment][:post_id]
+    if params[:user_id] != ''
+      @comment.user_id = params[:user_id]
+    else
+      @comment.user_id = User.create(username: params[:comment][:user_attributes][:username]).id
+    end
+    @comment.save
+    redirect_to post_path(@comment.post)
   end
 
   private
 
   def comment_params
-    params.require(:comment).permit(:content, :post_id, :user_id, user_attributes:[:username])
+    params.require(:comment).permit(:content, :post_id, :user_id, user_attributes: [:username])
   end
 end
